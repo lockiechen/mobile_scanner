a390a94a55f38b36e912108cde4980fc36f1a493
lockiechen <lockiechen@tencent.com>
2023-04-12 17:40:40 +0800

    add

--- ios/Classes/MobileScanner.swift
+++ ios/Classes/MobileScanner.swift
@@ -138,8 +138,8 @@ public class MobileScanner: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
             if device.isFocusModeSupported(.continuousAutoFocus) {
                 device.focusMode = .continuousAutoFocus
             }
-            if #available(iOS 15.4, *) , device.isFocusModeSupported(.autoFocus){
-                device.automaticallyAdjustsFaceDrivenAutoFocusEnabled = false
+            if #available(iOS 15.4, *) , device.isFocusModeSupported(.autoFocus) {
+                // device.automaticallyAdjustsFaceDrivenAutoFocusEnabled = false
             }
             device.unlockForConfiguration()
         } catch {}
